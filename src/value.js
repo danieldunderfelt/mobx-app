344ee86f15c7ac62824ac1d1096f5ecdee867115
srigi <srigi@srigi.sk>
2017-11-10 13:26:58 +0100

    Import Lodash submodules, fixes #2
    You can directly access individual functions as sub-modules from the main lodash module.
    Only the parts of Lodash you need will be included in your build and nothing else.

--- src/value.js
+++ src/value.js
@@ -1,12 +1,12 @@
 import { action, extendObservable } from 'mobx'
-import _ from 'lodash'
+import upperFirst from 'lodash/upperFirst'
 
 export default (state, property, initial = null) => {
 
   const setValue = action(`Value ${property} - Set value`, (value = initial) => state[property] = value )
   const extendValue = action(`Value ${property} - Extend value`, (value = initial) => extendObservable(state[property], value))
 
-  const methodSuffix = _.upperFirst(property)
+  const methodSuffix = upperFirst(property)
 
   // Because why not yeah
   return {
